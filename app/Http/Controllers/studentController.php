4ee1c01ea694ea2bdc30d4783d1a97d7897717fb
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2024-09-24 13:45:28 +0530

    validation form

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -4,20 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student2;
+use Illuminate\Support\Facades\Validator;
 
 class studentController extends Controller
 {
-    public function createStudent(){
+    public function createStudent()
+    {
         return view('students.create');
     }
 
-    public function viewStudent(){
+    public function viewStudent()
+    {
         return view('students.index');
     }
 
-    public function storeStudent(request $request){
-        $student =  new Student2();
+    public function storeStudent(request $request)
+    {
 
+        $rules = [
+            'firstName' => 'required|string',
+            'address' => 'required|string',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+
+        // 1st method insert
+        $student = new Student2();
         $student->first_name = $request->firstName;
         $student->last_name = $request->lastName;
         $student->contact_number = $request->contactNumber;
@@ -25,6 +42,8 @@ class studentController extends Controller
         $student->dob = $request->dob;
         $student->save();
 
+        // 2nd method insert(+fillable properties)
+        // $student2 = Student2::create($request->all());
         return ("Success");
 
     }
--- app/Models/student2.php
+++ app/Models/student2.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class student2 extends Model
 {
     use HasFactory;
+
+    // 2nd method fillable properties
+    // protected $fillable = [
+    //     'first_name','last_name','contact_number','address','dob'
+    // ];
 }
